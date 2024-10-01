b02831d2e7694d28df7f520628cf5ea49627fd07
daniel lance <poorfoolspencil@gmail.com>
2024-10-01 11:08:41 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.querySelector("main#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Daniel is the champion";
+
+newElement.textContent = "Daniel is the champion";
+
+document.body.append(newHeader);
